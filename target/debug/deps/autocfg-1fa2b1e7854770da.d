fd14b083d78faf8f8eb014620b91827075eb24f1
mukund <mukundt513@gmail.com>
2025-06-23 23:57:25 +0530

    http server in rust init cargo worksapce with rust

---
+++ target/debug/deps/autocfg-1fa2b1e7854770da.d
@@ -0,0 +1,10 @@
+C:\Users\thaku\beeterTime-rust\target\debug\deps\libautocfg-1fa2b1e7854770da.rmeta: C:\Users\thaku\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\autocfg-1.5.0\src\lib.rs C:\Users\thaku\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\autocfg-1.5.0\src\error.rs C:\Users\thaku\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\autocfg-1.5.0\src\rustc.rs C:\Users\thaku\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\autocfg-1.5.0\src\version.rs
+
+C:\Users\thaku\beeterTime-rust\target\debug\deps\libautocfg-1fa2b1e7854770da.rlib: C:\Users\thaku\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\autocfg-1.5.0\src\lib.rs C:\Users\thaku\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\autocfg-1.5.0\src\error.rs C:\Users\thaku\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\autocfg-1.5.0\src\rustc.rs C:\Users\thaku\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\autocfg-1.5.0\src\version.rs
+
+C:\Users\thaku\beeterTime-rust\target\debug\deps\autocfg-1fa2b1e7854770da.d: C:\Users\thaku\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\autocfg-1.5.0\src\lib.rs C:\Users\thaku\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\autocfg-1.5.0\src\error.rs C:\Users\thaku\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\autocfg-1.5.0\src\rustc.rs C:\Users\thaku\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\autocfg-1.5.0\src\version.rs
+
+C:\Users\thaku\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\autocfg-1.5.0\src\lib.rs:
+C:\Users\thaku\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\autocfg-1.5.0\src\error.rs:
+C:\Users\thaku\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\autocfg-1.5.0\src\rustc.rs:
+C:\Users\thaku\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\autocfg-1.5.0\src\version.rs:
---
+++ target/debug/deps/smallvec-71035298b547e34b.d
@@ -0,0 +1,7 @@
+C:\Users\thaku\beeterTime-rust\target\debug\deps\libsmallvec-71035298b547e34b.rmeta: C:\Users\thaku\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\smallvec-1.15.1\src\lib.rs
+
+C:\Users\thaku\beeterTime-rust\target\debug\deps\libsmallvec-71035298b547e34b.rlib: C:\Users\thaku\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\smallvec-1.15.1\src\lib.rs
+
+C:\Users\thaku\beeterTime-rust\target\debug\deps\smallvec-71035298b547e34b.d: C:\Users\thaku\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\smallvec-1.15.1\src\lib.rs
+
+C:\Users\thaku\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\smallvec-1.15.1\src\lib.rs:
