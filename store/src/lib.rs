74b75f26963009028cb13b293674884f5a0b06fb
mukund <mukundt513@gmail.com>
2025-07-01 02:08:41 +0530

    header , json pasing using  serde and use the store library use to store presist data using

--- store/src/lib.rs
+++ store/src/lib.rs
@@ -1,1 +1,17 @@
 
+
+pub struct Store{
+    // This struct will hold the database connection or any other state needed for the store
+}
+
+impl Store {
+    pub fn create_user(&self){
+
+    }
+    pub fn create_website(&self) -> String {
+        // Here you would typically insert the website into the database and return its ID
+        // For now, we will just return a dummy ID
+        String::from("1")
+
+    }
+}
---
+++ target/debug/deps/api-b20717b2c6c076a5.d
@@ -0,0 +1,6 @@
+C:\Users\thaku\beeterTime-rust\target\debug\deps\api-b20717b2c6c076a5.exe: api\src\main.rs api\src\request_input.rs
+
+C:\Users\thaku\beeterTime-rust\target\debug\deps\api-b20717b2c6c076a5.d: api\src\main.rs api\src\request_input.rs
+
+api\src\main.rs:
+api\src\request_input.rs:
